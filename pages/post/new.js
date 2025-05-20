e995171e7b5bcf7e777258058c08c09713665898
Sc0tticus <ssinger303@gmail.com>
2025-05-20 14:54:57 -0600

    wiring up OpenAI integration

--- pages/post/new.js
+++ pages/post/new.js
@@ -4,9 +4,20 @@ import { AppLayout } from "../../components/AppLayout/AppLayout";
 export default function NewPost(props) {
   console.log("NEW POST PROPS");
 
+  const handleClick = async () => {
+    const response = await fetch(`/api/generatePost`, {
+      method: "POST",
+    });
+    const json = await response.json();
+    console.log("RESULT", json);
+  };
+
   return (
     <div>
       <h1>this is the new post page</h1>
+      <button className="btn" onClick={handleClick}>
+        Generate
+      </button>
     </div>
   );
 }
